1816099aa31d2c97e44d0e563bc307ccdb0b1b96
AKaralis <project.igoncharov@yandex.ru>
2023-09-02 23:32:35 +0300

    1 commit

--- statistic/stat_client/src/main/java/ru/practicum/ewm/statistics/client/controller/StatisticClientController.java
+++ statistic/stat_client/src/main/java/ru/practicum/ewm/statistics/client/controller/StatisticClientController.java
@@ -1,41 +1,42 @@
-package ru.practicum.ewm.statistics.server.controller;
+package ru.practicum.ewm.statistics.client.controller;
 
 import lombok.RequiredArgsConstructor;
 import org.springframework.format.annotation.DateTimeFormat;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Controller;
+import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseStatus;
-import org.springframework.web.bind.annotation.RestController;
-import org.springframework.http.HttpStatus;
+import ru.practicum.ewm.statistics.client.client.StatisticsClient;
 import ru.practicum.ewm.statistics.dto.RequestHitDto;
-import ru.practicum.ewm.statistics.dto.ResponseHitDto;
-import ru.practicum.ewm.statistics.server.service.StatisticService;
-import ru.practicum.ewm.util.aspect.log.ToLog;
 
+import javax.validation.Valid;
 import java.time.LocalDateTime;
 import java.util.List;
 
 import static ru.practicum.ewm.util.constant.Constants.DATE_TIME_FORMAT;
 
-@RestController
+@Controller
 @RequiredArgsConstructor
-@ToLog
-public class StatisticsController {
-    private final StatisticService service;
+@Validated
+public class StatisticClientController {
+    private final StatisticsClient client;
 
     @PostMapping("/hit")
     @ResponseStatus(HttpStatus.CREATED)
-    public void createHit(@RequestBody RequestHitDto hitDto) {
-        service.createHit(hitDto);
+    public ResponseEntity<Object> createHit(@RequestBody @Valid RequestHitDto hitDto) {
+        return client.postHit(hitDto);
     }
 
     @GetMapping("/stats")
-    public List<ResponseHitDto> getStats(@RequestParam(name = "start") @DateTimeFormat(fallbackPatterns = DATE_TIME_FORMAT) LocalDateTime start,
+    public ResponseEntity<Object> getStats(@RequestParam(name = "start") @DateTimeFormat(fallbackPatterns = DATE_TIME_FORMAT) LocalDateTime start,
                                          @RequestParam(name = "end") @DateTimeFormat(fallbackPatterns = DATE_TIME_FORMAT) LocalDateTime end,
                                          @RequestParam(name = "uris", required = false) List<String> uris,
                                          @RequestParam(name = "unique", defaultValue = "false") boolean unique) {
-        return service.getStats(start, end, uris, unique);
+        return client.getStats(start, end, uris, unique);
     }
 }
