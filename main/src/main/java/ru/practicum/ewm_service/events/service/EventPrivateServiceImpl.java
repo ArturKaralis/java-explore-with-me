0ca0e5183873e646b71ced16633ab1bf04f2f0d0
ArturKaralis <106488258+ArturKaralis@users.noreply.github.com>
2023-09-13 18:42:02 +0300

    #17

--- main/src/main/java/ru/practicum/ewm_service/events/service/EventPrivateServiceImpl.java
+++ main/src/main/java/ru/practicum/ewm_service/events/service/EventPrivateServiceImpl.java
@@ -20,7 +20,7 @@ import ru.practicum.ewm_service.requests.dto.ParticipationRequestDto;
 import ru.practicum.ewm_service.requests.mapper.RequestMapper;
 import ru.practicum.ewm_service.requests.model.ParticipationRequest;
 import ru.practicum.ewm_service.requests.repository.RequestRepository;
-import ru.practicum.ewm_service.statclient.StatisticsClient;
+import ru.practicum.ewm_service.statclient.Client;
 import ru.practicum.ewm_service.user.model.User;
 import ru.practicum.ewm_service.user.repository.UserRepository;
 import ru.practicum.ewm_service.utils.Status;
@@ -43,7 +43,7 @@ public class EventPrivateServiceImpl implements EventPrivateService {
     private final CategoryRepository categoryRepository;
     private final RequestRepository requestRepository;
     private final LocationRepository locationRepository;
-    private final StatisticsClient statClient;
+    private final Client statClient;
 
     @Override
     @Transactional(readOnly = true)
