bac185422db444d00a6ce5ce7322e466affc3936
AKaralis <project.igoncharov@yandex.ru>
2023-09-14 00:06:18 +0300

    Project part 2

--- main/src/main/java/ru/practicum/ewm/EwmMainServer.java
+++ main/src/main/java/ru/practicum/ewm/EwmMainServer.java
@@ -2,6 +2,6 @@ package ru.practicum.ewm;
 
 public class EwmMainServer {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        System.out.println("Вторая часть дипломной работы!");
     }
 }
