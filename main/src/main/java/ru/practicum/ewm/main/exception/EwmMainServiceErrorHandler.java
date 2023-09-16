1505b8c6dd3f0a6828f0b259aad71a7576a55e31
ArturKaralis <106488258+ArturKaralis@users.noreply.github.com>
2023-09-16 15:15:13 +0300

    попытка 3. v3

--- main/src/main/java/ru/practicum/ewm/main/exception/EwmMainServiceErrorHandler.java
+++ main/src/main/java/ru/practicum/ewm/main/exception/EwmMainServiceErrorHandler.java
@@ -22,6 +22,7 @@ import java.util.stream.Collectors;
 @Slf4j
 public class EwmMainServiceErrorHandler {
 
+
     @ExceptionHandler
     @ResponseStatus(HttpStatus.BAD_REQUEST)
     public ErrorResponseList handleConstraintViolationException(ConstraintViolationException e) {
